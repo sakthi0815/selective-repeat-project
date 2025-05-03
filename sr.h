3e02017e37db8d4a54452363be238e7fceb8d919
sakthi0815 <162859828+sakthi0815@users.noreply.github.com>
2025-05-04 00:31:37 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,31 +1,54 @@
-#ifndef SR_H
-#define SR_H
+#ifndef _SR_H_
+#define _SR_H_
 
-#define WINDOW_SIZE 4
-#define TIMEOUT 16.0
-#define PAYLOAD_SIZE 20
-
+/* Define structures to match emulator.h */
 struct msg {
-    char data[PAYLOAD_SIZE];
+    char data[20];
 };
 
 struct pkt {
     int seqnum;
     int acknum;
     int checksum;
-    char payload[PAYLOAD_SIZE];
+    char payload[20];
 };
 
-extern struct pkt window[WINDOW_SIZE];
+/* Event structure for compatibility with emulator */
+typedef struct {
+    float time;
+    int type;
+    int entity;
+    struct pkt packet;
+    struct msg message;
+} event;
 
-int compute_checksum(struct pkt packet);
-int is_corrupt(struct pkt packet);
+#define WINDOW_SIZE 6
+#define TIMEOUT 40.0  // Increased to 40.0 to improve delivery
+#define MAX_SEQ 31
 
-void A_output(struct msg message);
-void A_input(struct pkt packet);
-void A_timerinterrupt(void);
-void A_init(void);
-void B_input(struct pkt packet);
-void B_init(void);
+/* Function prototypes using struct pkt and struct msg */
+extern float get_clocktime(void);
+extern void tolayer3(int entity, struct pkt packet);
+extern void tolayer5(int entity, char *data);
+extern void starttimer(int entity, float increment);
+extern void stoptimer(int entity);
+extern void generate_next_arrival(void);
+extern void init(void);
+extern int getwinsize(void);
+
+extern int TRACE;
+extern int nsim;
+extern int nsimmax;
+extern float lossprob;
+extern float corruptprob;
+extern float lambda;
+extern int ntolayer3;
+extern int nlost;
+extern int ncorrupt;
+extern int window_full;
+extern int packets_resent;
+extern int total_ACKs_received;
+extern int new_ACKs;
+extern int packets_received;
 
 #endif
